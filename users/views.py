308eb05aae35ab8de5053676bcf13898ffa33928
NTK1100 <trungkienabc2004@gmail.com>
2025-04-19 17:15:24 +0700

    Kien add chapter 20

--- users/views.py
+++ users/views.py
@@ -1,21 +1,24 @@
+from django.shortcuts import render
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    """Đăng ký người dùng mới"""
+    """Register a new user."""
     if request.method != 'POST':
-        # Hiển thị form đăng ký trống
+        # Display blank registration form.   
         form = UserCreationForm()
     else:
-        # Xử lý form đã điền
+        # Process completed form.
         form = UserCreationForm(data=request.POST)
+        
         if form.is_valid():
             new_user = form.save()
-            # Đăng nhập và chuyển hướng về trang chủ
+            # Log the user in and then redirect to home page.
             login(request, new_user)
             return redirect('learning_logs:index')
-    
-    # Hiển thị form trống hoặc không hợp lệ
+
+    # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'registration/register.html', context)
