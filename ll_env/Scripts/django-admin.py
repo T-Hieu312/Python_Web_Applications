16ecc20220ab949cbad9b09f67796f518c824ae7
Bùi Văn Minh <68287460+toilaminhbui@users.noreply.github.com>
2025-04-19 22:29:55 +0700

    Delete ll_env directory

--- ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\python tutorials\python crash course\part ii\web application\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
